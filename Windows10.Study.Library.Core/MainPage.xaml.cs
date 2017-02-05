2f127f1b1d6754bd0a4340cb26c9573053f7b592
damingerdai <937765518@qq.com>
2017-02-05 20:37:17 +0800

    init this library

---
+++ Windows10.Study.Library.Core/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows10.Study.Library.Background;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Windows10.Study.Library.Core
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Windows10开发学习";
+
+        List<Scenario> scenarios = new List<Scenario>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = (splitView.IsPaneOpen) ? false : true;
+        }
+    }
+}
